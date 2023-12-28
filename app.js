d905f04450f4ac37dc2447fbcd2ae651abffaf03
EC2 Default User <ec2-user@ip-172-31-93-235.ec2.internal>
2023-12-28 15:39:25 +0000

    add websocket

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const http = require("http");
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-http.createServer(function(req, res) {
-	res.write(`<!DOCTYPE html><h1>Hello world!</h1>`);
-	res.end();
-}).listen(3000);
-console.log("Server started on port 3000");
+app.get("/", (req, res) => {
+	res.sendFile("index.html", {root: __dirname});
+});
+
+const server = http.createServer();
+server.on("request", app);
+server.listen(PORT, () => {
+	console.log(`server starter on port ${PORT}`);
+});
+
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({server});
+
+wss.on("connection", function connection(ws) {
+	const numClients = wss.clients.size;
+	console.log("Clients connected", numClients);
+
+	wss.broadcast(`Current visitors: ${numClients}`);
+
+	if (ws.readyState === ws.OPEN) {
+		ws.send("Welcome to my server");
+	}
+
+	ws.on("close", function close() {
+		wss.broadcast(`Current visitors: ${numClients}`);
+		console.log("A client has disconnected");
+	});
+});
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(client => client.send(data));
+}
